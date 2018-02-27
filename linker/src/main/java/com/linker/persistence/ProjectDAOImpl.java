fd189a96e9dbbf017098abd06375bc9bccf7ed81
leepilju <pilju413@gmail.com>
2018-02-27 10:11:15 +0900

    프로젝트 추가,수정(상태값변경, 프로젝트이름변경), 프로젝트리스트 테스트코드완료

--- linker/src/main/java/com/linker/persistence/ProjectDAOImpl.java
+++ linker/src/main/java/com/linker/persistence/ProjectDAOImpl.java
@@ -1,5 +1,7 @@
 package com.linker.persistence;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import org.apache.ibatis.session.SqlSession;
@@ -15,9 +17,23 @@ public class ProjectDAOImpl implements ProjectDAO {
 	@Inject
 	SqlSession session;
 
+	// 프로젝트 생성
 	@Override
 	public int createProject(ProjectVO vo) throws Exception {
 		return session.insert(namespace+".createProject", vo);
+	}
+
+	// 프로젝트 리스트
+	@Override
+	public List<ProjectVO> listProject(int teamID) throws Exception {
+		return session.selectList(namespace+".listProject", teamID);
+	}
+
+
+	// 프로젝트 달성&프로젝트 가리기(숨기기)&프로젝트 이름변경
+	@Override
+	public int modifyProject(ProjectVO vo) throws Exception {
+		return session.update(namespace+".modifyProject", vo);
 	}
 
 }
--- linker/src/main/java/com/linker/service/ProjectServiceImpl.java
+++ linker/src/main/java/com/linker/service/ProjectServiceImpl.java
@@ -1,5 +1,7 @@
 package com.linker.service;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import org.springframework.stereotype.Service;
@@ -13,9 +15,23 @@ public class ProjectServiceImpl implements ProjectService {
 	@Inject
 	ProjectDAO dao;
 
+	// 프로젝트 생성
 	@Override
 	public int createProject(ProjectVO vo) throws Exception {
 		return dao.createProject(vo);
 	}
+
+	// 프로젝트 리스트
+	@Override
+	public List<ProjectVO> listProject(int teamID) throws Exception {
+		return dao.listProject(teamID);
+	}
+
+	// 프로젝트 달성&프로젝트 가리기(숨기기)&프로젝트 이름변경
+	@Override
+	public int modifyProject(ProjectVO vo) throws Exception {
+		return dao.modifyProject(vo);
+	}
+
 
 }
